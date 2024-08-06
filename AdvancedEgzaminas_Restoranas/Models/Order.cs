623afe4a64a11553dcb70028b352270632cb78c8
Gaudrius <gaudriusk@gmail.com>
2024-08-06 21:39:17 +0300

    Implemented OrderService method.

--- AdvancedEgzaminas_Restoranas/Models/Order.cs
+++ AdvancedEgzaminas_Restoranas/Models/Order.cs
@@ -2,10 +2,19 @@
 {
     public class Order
     {
-        public int ID { get; set; }
+        public Guid ID { get; set; }
         public Table Table { get; set; }
         public List<Product> Products { get; set; }
         public decimal TotalAmount { get; set; }
         public DateTime OrderTime { get; set; }
+
+        public Order(Table table, List<Product> products, decimal totalAmount, DateTime orderTime)
+        {
+            ID = Guid.NewGuid();
+            Table = table;
+            Products = products;
+            TotalAmount = totalAmount;
+            OrderTime = orderTime;
+        }
     }
 }
--- AdvancedEgzaminas_Restoranas/Services/OrderService.cs
+++ AdvancedEgzaminas_Restoranas/Services/OrderService.cs
@@ -3,15 +3,33 @@ using AdvancedEgzaminas_Restoranas.Services.Interfaces;
 
 namespace AdvancedEgzaminas_Restoranas.Services
 {
+    // save to orders.csv
     public class OrderService : IOrderService
     {
-        private List<Table> _orders;
+        public OrderService(List<Order> orders, IDataAccess dataAccess, ITableService tableService, string filePath)
+        {
+            _orders = orders;
+            _dataAccess = dataAccess;
+            _tableService = tableService;
+            //_filePath = filePath;
+        }
+
+        private List<Order> _orders;
         private readonly IDataAccess _dataAccess;
-        private readonly string _filePath;
+        private readonly ITableService _tableService;
+        //private readonly string _filePath;
 
         public Order CreateOrder(int tableNumber, List<Product> products)
         {
-            throw new NotImplementedException();
+            Table table = _tableService.GetTable(tableNumber);
+
+            decimal totalPrice = 0;
+            foreach (var product in products)
+            {
+                totalPrice = product.Price;
+            }
+
+            return new Order(table, products, totalPrice, DateTime.Now);
         }
     }
 }
