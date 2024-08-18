e525ce99ce38562eaaefa2fbe211a12d3b4da9b2
Gaudrius <gaudriusk@gmail.com>
2024-08-19 00:40:21 +0300

    Added GetOrder() tests.

--- AdvancedEgzaminas_RestoranasTests/Services/OrderServiceTests.cs
+++ AdvancedEgzaminas_RestoranasTests/Services/OrderServiceTests.cs
@@ -9,16 +9,16 @@ namespace AdvancedEgzaminas_Restoranas.Services.Tests
     public class OrderServiceTests
     {
         private IOrderService _orderService;
-        private IDataAccess _dataAccess;
+        private FakeDataAccess _fakeDataAccess;
 
         [TestInitialize()]
         public void Setup()
         {
-            _dataAccess = new DataAccess.DataAccess();
+            _fakeDataAccess = new FakeDataAccess();
             _orderService = new OrderService(
-                _dataAccess,
+                _fakeDataAccess,
                 new FakeTableService(),
-                new ProductService(_dataAccess, "drinksFakePath", "foodFakePath"),
+                new ProductService(_fakeDataAccess, "drinksFakePath", "foodFakePath"),
                 new UserInterface(),
                 "ordersFakePath");
         }
@@ -85,6 +85,101 @@ namespace AdvancedEgzaminas_Restoranas.Services.Tests
 
             // Assert
             Assert.AreEqual(tableNumber, result.Table.Number);
+        }
+
+        [TestMethod]
+        public void GetOrder_ExistingTableNumber_ReturnsCorrectOrder()
+        {
+            // Arrange
+            int tableNumber = 1;
+            var expectedOrder = new Order(new Table(tableNumber, 4), new List<Product>(), 100m, DateTime.Now);
+            _fakeDataAccess.SetTestOrders(new List<Order> { expectedOrder });
+
+            // Act
+            var result = _orderService.GetOrder(tableNumber);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(tableNumber, result.Table.Number);
+            Assert.AreEqual(expectedOrder.TotalAmount, result.TotalAmount);
+        }
+
+        [TestMethod]
+        public void GetOrder_NonExistingTableNumber_ReturnsNull()
+        {
+            // Arrange
+            int tableNumber = 99;
+            var testOrders = new List<Order>
+            {
+                new Order(new Table(1, 4), new List<Product>(), 100m, DateTime.Now),
+                new Order(new Table(2, 2), new List<Product>(), 50m, DateTime.Now)
+            };
+            _fakeDataAccess.SetTestOrders(testOrders);
+
+            // Act
+            var result = _orderService.GetOrder(tableNumber);
+
+            // Assert
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void GetOrder_EmptyOrderList_ReturnsNull()
+        {
+            // Arrange
+            _fakeDataAccess.SetTestOrders(new List<Order>());
+
+            // Act
+            var result = _orderService.GetOrder(1);
+
+            // Assert
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void GetOrder_DataAccessThrowsException_PropagatesException()
+        {
+            // Arrange
+            _fakeDataAccess.ShouldThrowException = true;
+
+            // Act
+            _orderService.GetOrder(1);
+        }
+    }
+
+    public class FakeDataAccess : IDataAccess
+    {
+        private List<Order> _testOrders;
+        public bool ShouldThrowException { get; set; }
+
+        public void SetTestOrders(List<Order> orders)
+        {
+            _testOrders = orders;
+        }
+
+        public List<T> ReadJson<T>(string filePath)
+        {
+            if (ShouldThrowException)
+            {
+                throw new Exception("Simulated data access error");
+            }
+            return _testOrders as List<T>;
+        }
+
+        public List<T> ReadCsv<T>(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteCsv<T>(string filePath, List<T> data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteJson<T>(string filePath, List<T> data)
+        {
+            throw new NotImplementedException();
         }
     }
 
